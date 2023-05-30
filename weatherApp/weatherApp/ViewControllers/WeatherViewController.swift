b8b96ccdc86ca8d998e5cddcb0491a67c0b9a4d5
anevodchikova <anevodchikova29@gmail.com>
2023-05-30 21:59:08 +0300

    start project

---
+++ weatherApp/weatherApp/ViewControllers/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Anastasia Nevodchikova on 30.05.2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
